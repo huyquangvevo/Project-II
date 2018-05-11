00aa6446987a23861c1cd314c7818a49965c4613
huyquangvevo <quanghuy27101997@gmail.com>
2018-05-11 11:30:41 +0700

    Update

--- index.js
+++ index.js
@@ -2,16 +2,38 @@ var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
 var java = require("java");
+//const http2 = require('http2');
+
+var server = require('http').Server(app);
+//var server = http2.createSecureServer(app);
+
+var io = require('socket.io')(server);
+io.on('connection',function(socket){
+
+    console.log('Socket io');
+
+});
+
+
+
+server.listen(3000,function(){
+    console.log("OK connection!");
+});
+
+
 java.classpath.push("./java/APIClient-0.0.1-SNAPSHOT.jar");
 java.callStaticMethodSync("com.huy.APIClient.ClientAPI","connectToRepository",false);
 
 var urlencodedParser = bodyParser.urlencoded({extended:false});
 
-app.listen(3000);
+//app.listen(3000);
 
-app.set("view engine","ejs");
-app.set("views","./views");
+//app.set("view engine","ejs");
+//app.set("views","./views");
 app.use(express.static(__dirname + '/public'));
+
+app.use(express.static(__dirname));
+
 
 app.get('/', function(req,res){
     res.render("home",{a:"Huy",data:"ok"});
@@ -20,14 +42,19 @@ app.get('/', function(req,res){
 
 app.post('/',urlencodedParser, function(req,res){
     var result;
-    if(req.body.propertyInstance != "")
+   if(req.body.propertyInstance != "" && req.body.propertyInstance != null)
        // result = java.callStaticMethodSync("com.huy.APIClient.ClientAPI","getPropertyOfInstance",req.body.propertyInstance);
-       result = java.callStaticMethodSync("com.huy.APIClient.ClientAPI","getPropertyOfInstance","<http://dbpedia.org/ontology/Province>");
-    if(req.body.instanceClass != "")
+       result = java.callStaticMethodSync("com.huy.APIClient.ClientAPI","getPropertyOfInstance",req.body.propertyInstance);
+    if(req.body.instanceClass != "" && req.body.instanceClass != null)
         result = java.callStaticMethodSync("com.huy.APIClient.ClientAPI","getInstanceOfClass",req.body.instanceClass);
-    if(req.body.allInstance != "")
+    if(req.body.allInstance != "" && req.body.allInstance != null)
         result = java.callStaticMethodSync("com.huy.APIClient.ClientAPI","getInstance",req.body.allInstance,true);
      //   console.log(result);
+
+     if(req.body.automatic == "yes"){
+         result = java.callStaticMethodSync("com.huy.APIClient.ClientAPI","getInstanceOfClass","owl:Class");
+        console.log("Loading Jmeter")
+        }
        
    // result = java.callStaticMethodSync("com.huy.APIClient.ClientAPI","getPropertyOfInstance",req.body.propertyInstance);
     
